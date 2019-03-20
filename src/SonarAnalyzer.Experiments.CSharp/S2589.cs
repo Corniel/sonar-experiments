a359c8b2932de7dd0d04c22148cb276817b77b5d
Corniel Nobel <mail@corniel.nl>
2019-03-20 21:52:34 +0100

    Added S2589.

---
+++ src/SonarAnalyzer.Experiments.CSharp/S2589.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace SonarAnalyzer.Experiments.CSharp
+{
+    public class S2589
+    {
+        public bool ListContainsAtLeastOnePositiveAndNoNegatives(List<int> list)
+        {
+            bool containspositive = false;
+            bool containsnegative = false;
+
+            foreach (int value in list)
+            {
+                if (value > 0)
+                {
+                    containspositive = true;
+                }
+                else if (value < 0)
+                {
+                    containsnegative = true;
+                }
+            }
+
+            return containspositive && !containsnegative;
+        }
+    }
+}

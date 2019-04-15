803ace1d43bf494bbfeebb152f4b03edbaaa857f
Corniel Nobel <mail@corniel.nl>
2019-04-15 10:05:23 +0200

    Added HassFlag proposal.

---
+++ src/SonarAnalyzer.Experiments.CSharp/HasFlag.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SonarAnalyzer.Experiments.CSharp
+{
+public class HasFlag
+{
+    [Flags]
+    public enum Flagged
+    {
+        None = 0,
+        UnionJack = 1,
+        StarSpangledBanner = 2,
+    }
+
+    public void Test(Flagged flags, LoaderOptimization optimization)
+    {
+        var noFlag = optimization.HasFlag(LoaderOptimization.MultiDomain); // none-compliant. LoaderOptimization is not a flags enum.
+        var hasNone = flags.HasFlag(Flagged.None); //none-compliant, None is not a flag value.
+        var hasOther = flags.HasFlag(LoaderOptimization.SingleDomain); // not-compliant, enum types don't match
+        var hasUS = flags.HasFlag(Flagged.StarSpangledBanner); // compliant
+    }
+}
+}

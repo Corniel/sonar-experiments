58b2ab37807dba74396435501bacd69220cb7fa1
Corniel Nobel <mail@corniel.nl>
2019-04-19 23:02:19 +0200

    Add rule suggestions.

---
+++ src/SonarAnalyzer.Experiments.CSharp/CharBasedEnumsShouldNotBeDecoratedWithFlags.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SonarAnalyzer.Experiments.CSharp
+{
+[Flags]
+public enum CharBasedEnumsShouldNotBeDecoratedWithFlags // noncompliant
+{
+    A = 'A',
+    B = 'B',
+    C = 'C',
+    ABC = A | B | C,
+}
+}
---
+++ src/SonarAnalyzer.Experiments.CSharp/DontSortUncomparebleLists.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SonarAnalyzer.Experiments.CSharp
+{
+    public class DontSortUncomparebleLists
+    {
+        public DontSortUncomparebleLists()
+        {
+            var objects = new List<object>();
+            objects.Sort(); // noncompliant. object does not implement IComparable<T>
+            objects.Sort(Comparer<object>.Default); // compliant. The comparer should no what to do.
+
+            var ints = new List<int>();
+            ints.Sort(); // compliant. int implements IComparable<int>
+        }
+
+        public static void Sort<T>(List<T> list)
+        {
+            list.Sort(); // noncompliant. object might not implement IComarable<T>
+        }
+
+        public static void Sort2<T>(List<T> list)
+            where T : IComparable<T>
+        {
+            list.Sort(); // compliant. T implements IComparable<T>
+        }
+    }
+}

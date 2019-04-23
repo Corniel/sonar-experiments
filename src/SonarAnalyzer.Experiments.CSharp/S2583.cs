8ccc48b85998428f34b5543ac2d91b2578438e7d
Corniel Nobel <mail@corniel.nl>
2019-04-23 09:10:46 +0200

    False positive on S2583.

---
+++ src/SonarAnalyzer.Experiments.CSharp/S2583.cs
@@ -0,0 +1,18 @@
+﻿namespace SonarAnalyzer.Experiments.CSharp
+{
+    public class S2583
+    {
+        public int FalsePositiveOnStringEmpty(string input)
+        {
+            if(input == null)
+            {
+                return -1;
+            }
+            if (string.IsNullOrEmpty(input)) // compliant. String.Empty is different than (string)null.
+            {
+                return -2;
+            }
+            return 0;
+        }
+    }
+}

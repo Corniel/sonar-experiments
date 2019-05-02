3481a91f69d25696742dbe91f52f214ebe27b624
Corniel Nobel <mail@corniel.nl>
2019-05-02 12:53:10 +0200

    False positive on S4462 for ContiueWith().

---
+++ src/SonarAnalyzer.Experiments.CSharp/S4462.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SonarAnalyzer.Experiments.CSharp
+{
+    public class S4462
+    {
+        public Task<S4462> Run(Func<Task<S4462>> continuation, CancellationToken token)
+        {
+            return continuation().ContinueWith((action) =>
+            {
+                return action.Result; // Compliant, ContinueWith() enforces to do so.
+            });
+        }
+    }
+}

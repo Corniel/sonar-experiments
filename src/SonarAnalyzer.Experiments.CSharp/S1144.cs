b215bd939b8f85eed319a68da2554d4363be04f5
Corniel Nobel <mail@corniel.nl>
2019-05-02 11:18:32 +0200

    False positive on S1144.

---
+++ src/SonarAnalyzer.Experiments.CSharp/S1144.cs
@@ -0,0 +1,17 @@
+﻿namespace SonarAnalyzer.Experiments.CSharp
+{
+    public class S1144
+    {
+        public void M()
+        {
+            OtherClass oc = new OtherClass();
+            (oc.Name, oc.Type) = ("A", "B");
+        }
+
+        class OtherClass
+        {
+            public string Name { get; set; } // Compliant, usage via a TupleExpression should be recognized.
+            public string Type { get; set; } // Compliant, usage via a TupleExpression should be recognized.
+        }
+    }
+}

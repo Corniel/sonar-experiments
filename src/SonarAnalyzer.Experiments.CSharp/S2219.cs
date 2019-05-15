5791c1c1cf6d4241183a1d9bd2b35f9ed24b65c8
Corniel Nobel <mail@corniel.nl>
2019-05-15 13:52:47 +0200

    S2219 what should be compliant.

---
+++ src/SonarAnalyzer.Experiments.CSharp/S2219.cs
@@ -0,0 +1,21 @@
+﻿namespace SonarAnalyzer.Experiments.CSharp
+{
+public class S2219
+{
+    public S2219(object arg)
+    {
+        if(arg == null) // Not-compliant
+        {
+        }
+        if(arg != null) // Not-compliant
+        {
+        }
+        if (arg is null) // Compliant
+        {
+        }
+        if (arg is object) // Compliant
+        {
+        }
+    }
+}
+}

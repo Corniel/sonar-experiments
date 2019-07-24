fafa5b368f486d34e365527e2945a8269743280f
Corniel Nobel <mail@corniel.nl>
2019-07-24 14:19:59 +0200

    Added S3717 issue.

---
+++ src/SonarAnalyzer.Experiments.CSharp/S3717.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SonarAnalyzer.Experiments.CSharp
+{
+public class S3717
+{
+    public void NotImplemented() => throw new NotImplementedException(); // not-compliant
+
+    public void NotImplementedInline()
+    {
+        throw new NotImplementedException(); // not
+    }
+}
+}

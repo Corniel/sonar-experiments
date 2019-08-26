c52fccc2569391ee4b95602e79e327f078aea815
Corniel Nobel <mail@corniel.nl>
2019-08-26 17:04:58 +0200

    Added DisposableExplicit.

---
+++ src/SonarAnalyzer.Experiments.CSharp/Disposable.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SonarAnalyzer.Experiments.CSharp
+{
+    public class Disposable : IDisposable // Should not raise S3881
+    {
+        void IDisposable.Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        /// <summary>Disposes the API test context.</summary>
+        protected virtual void Dispose(bool disposing) // Should not raise S2953
+        {
+            if (!isDisposed && disposing)
+            {
+                // dispose something.
+            }
+            isDisposed = true;
+        }
+        private bool isDisposed;
+    }
+}

9ce98af8165de02d3be7d428848c817cb31278b4
Corniel Nobel <mail@corniel.nl>
2019-10-02 11:24:22 +0200

    False positive on S4136.

---
+++ src/SonarAnalyzer.Experiments.CSharp/S4136.cs
@@ -0,0 +1,10 @@
+﻿using System; // Compliant, used by cref.
+
+namespace SonarAnalyzer.Experiments.CSharp
+{
+    public enum S4136
+    {
+        /// <summary><see cref="DateTime"/></summary>
+        DateTimeValue,
+    }
+}

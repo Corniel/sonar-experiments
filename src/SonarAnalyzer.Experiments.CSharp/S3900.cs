12bdda459728b094bf31fec05ae6597b76b179ba
Corniel Nobel <mail@corniel.nl>
2019-10-03 15:28:08 +0200

    Add two cases.

---
+++ src/SonarAnalyzer.Experiments.CSharp/S3900.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SonarAnalyzer.Experiments.CSharp
+{
+public static class S3900
+{
+    public static void Extension(List<string> list)
+    {
+        if (list.IsNotNullOrEmpty())
+        {
+            foreach (string item in list) // Compliant
+            {
+                // Do something
+            }
+        }
+    }
+
+    public static void Method(List<string> list)
+    {
+        if (HasAny(list))
+        {
+            foreach (string item in list) // Compliant
+            {
+                // Do something
+            }
+        }
+    }
+
+    private static bool HasAny<T>([ValidatedNotNull]IEnumerable<T> src)
+    {
+        if (src is null)
+        {
+            return false;
+        }
+        return src.Any();
+    }
+}
+public static class S3900Exentsions
+{
+    public static bool IsNotNullOrEmpty<T>([ValidatedNotNull]this IEnumerable<T> src)
+    {
+        if (src is null)
+        {
+            return false;
+        }
+        return src.Any();
+    }
+}
+
+[AttributeUsage(AttributeTargets.Parameter)]
+public sealed class ValidatedNotNullAttribute : Attribute { }
+}
---
+++ src/SonarAnalyzer.Experiments.CSharp/S4143.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace SonarAnalyzer.Experiments.CSharp
+{
+    public class S4143
+    {
+        public List<string> Concatenated()
+        {
+            var list = new List<string>();
+            list.Add("SomeText");
+            list.Add("\n");
+            list.Add("SomeText");
+            list.Add("\n");
+
+            return list;
+        }
+    }
+}

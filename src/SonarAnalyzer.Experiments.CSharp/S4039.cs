65d0257a12d204695258a5650752e3f8e4244089
Corniel Nobel <mail@corniel.nl>
2019-11-01 09:56:15 +0100

    Add S4039 example: https://github.com/SonarSource/sonar-dotnet/issues/2757

---
+++ src/SonarAnalyzer.Experiments.CSharp/S4039.cs
@@ -0,0 +1,28 @@
+﻿namespace SonarAnalyzer.Experiments.CSharp
+{
+    public class S4039 : S4039Double, I4039Int
+    {
+        public S4039(int value) : base(value) { }
+
+        int I4039Int.Value => (int)Value; // compliant
+
+        bool I4039Int.Add(int val) // compliant
+        {
+            Add(val);
+            return true;
+        }
+    }
+
+    public class S4039Double
+    {
+        public S4039Double(double val) => Value = val;
+
+        public double Value { get; private set; }
+        public void Add(int val) => Value += val;
+    }
+    public interface I4039Int
+    {
+        int Value { get; }
+        bool Add(int val);
+    }
+}

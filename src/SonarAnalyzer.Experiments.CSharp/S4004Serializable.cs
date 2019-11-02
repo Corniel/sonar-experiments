1cfc1b1564f4211eb91324e5e221824d3014420b
Corniel <mail@corniel.nl>
2019-11-02 14:38:03 +0100

    S4004 Serialization issue https://github.com/SonarSource/sonar-dotnet/issues/2762

---
+++ src/SonarAnalyzer.Experiments.CSharp/S4004Serializable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SonarAnalyzer.Experiments.CSharp
+{
+    [Serializable]
+    public class S4004Serializable
+    {
+        public List<int> Values { get; set; } // Compliant, [Serializable] requires a public setter.
+    }
+}

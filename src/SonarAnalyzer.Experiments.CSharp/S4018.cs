9d331cf29d07d5d2500661664265e029eaa21ab1
Corniel Nobel <mail@corniel.nl>
2020-02-19 20:21:09 +0100

    Some generic methods do not require a type parameter.

---
+++ src/SonarAnalyzer.Experiments.CSharp/S4018.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace SonarAnalyzer.Experiments.CSharp
+{
+    public static class S4018
+    {
+        public static T MyCast<T>(object obj) // compliant
+        {
+            return (T)obj;
+        }
+
+        public static T ObjectIs<T>(object obj) // compliant
+        {
+            if (obj is T casted)
+            {
+                return casted;
+            }
+            throw new InvalidOperationException();
+        }
+
+        public static T MyDefault<T>() // compliant
+        {
+            return default;
+        }
+    }
+}

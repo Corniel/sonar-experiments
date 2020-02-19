eddf2d10bdf36271a9f8cf7362ae9a4e8eda736c
Corniel Nobel <mail@corniel.nl>
2020-02-19 20:11:07 +0100

    Added S4049 case with returning IEnumerable<T>.

---
+++ src/SonarAnalyzer.Experiments.CSharp/S4049_IEnumerable.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace SonarAnalyzer.Experiments.CSharp
+{
+public class S4049_IEnumerable
+{
+    public IEnumerable<char> GetChars() => "some characters"; // not compliant, defacto a string.
+
+    public IEnumerable<char> GetCharsAsEnumerable() => "some characters".Where(ch => char.IsLetter(ch)); // compliant?!
+
+    public IEnumerable<int> GetNumbers() => new[] { 1, 2 }.Where(n => (n % 2) == 1); // compliant.
+
+    public List<string> GetStrings() => new List<string> { "hello", "world" }; // compliant.
+
+    public List<int> GetNumberList() => GetNumbers().ToList(); // compliant.
+}
+}

5c8c292c73951763e84e8cb61898ab46abb1ef28
Corniel Nobel <mail@corniel.nl>
2020-02-19 16:51:50 +0100

    False positive on S4049.

---
+++ src/SonarAnalyzer.Experiments.CSharp/S4049.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SonarAnalyzer.Experiments.CSharp
+{
+    public class S4049
+    {
+        private static readonly Random rnd = new Random();
+
+        [NoProperty]
+        public int GetRandom() => rnd.Next(); // compliant, the attribute is not allowed on properties.
+
+        [AttributeUsage(AttributeTargets.All ^ AttributeTargets.Property)]
+        private class NoPropertyAttribute : Attribute { }
+    }
+}

3c8f33e395c06342e8d8b931af0f6f26717a57c7
Corniel Nobel <mail@corniel.nl>
2020-02-26 22:28:56 +0100

    Added false positive and negative for S3937.

---
+++ src/SonarAnalyzer.Experiments.CSharp/S3937.cs
@@ -0,0 +1,13 @@
+﻿namespace SonarAnalyzer.Experiments.CSharp
+{
+public static class S3937
+{
+    public static readonly long Hexa2 = 0xFF_FF_FF_FF; // compliant
+    public static readonly long Hexa3 = 0x121_1F3_456; // non-compliant, hexadecimals should have groups of 2 or 4 
+    public static readonly long Hexa4 = 0xF_FFFF_FFFF; // compliant
+
+    public static readonly long Bin2 = 0b1_10_11_10_11; // compliant
+    public static readonly long Bin3 = 0b0_111_111_111; // non-compliant, binaries should have groups of 2 or 4 
+    public static readonly long Bin4 = 0b111_1101_1011; // compliant
+}
+}

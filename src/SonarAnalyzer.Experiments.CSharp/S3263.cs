8fa68ce39015f0a0888e7f734ba424e4f10a751a
Corniel Nobel <mail@corniel.nl>
2020-03-06 17:15:54 +0100

    Added S3263.

---
+++ src/SonarAnalyzer.Experiments.CSharp/S3263.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace SonarAnalyzer.Experiments.CSharp
+{
+public struct S3263
+{
+    public static readonly S3263 Zero;
+    public static readonly S3263 V42 = new S3263(42);
+
+    public static readonly IReadOnlyCollection<S3263> All = new[] { Zero, V42, }; // compliant, both values have been initialized.
+
+    public static readonly IReadOnlyCollection<S3263> Extra = new[] { Zero, V007, V42, }; // noncompliant, V007 has not been initalized.
+
+    public static readonly S3263 V007 = new S3263(7);
+
+    public S3263(int i) => Value = i;
+
+    public int Value { get; }
+}
+}
---
+++ test/SonarAnalyzer.Experiments.UnitTests/S3263Test.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using SonarAnalyzer.Experiments.CSharp;
+using System.Linq;
+
+namespace SonarAnalyzer.Experiments.UnitTests
+{
+    public class S3263Test
+    {
+        [Test]
+        public void Init()
+        {
+            var all = S3263.All.ToArray();
+
+            Assert.AreEqual("0, 42", string.Join(", ", all.Select(s => s.Value)));
+
+        }
+    }
+}

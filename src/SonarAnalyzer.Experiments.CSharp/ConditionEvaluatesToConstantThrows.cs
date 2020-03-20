454b3db9f31855217d78cdca7939f66c79cd26b1
Corniel Nobel <mail@corniel.nl>
2020-03-20 19:54:45 +0100

    AD00001 crash type.

---
+++ src/SonarAnalyzer.Experiments.CSharp/ConditionEvaluatesToConstantThrows.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace SonarAnalyzer.Experiments.CSharp
+{
+    public class ConditionEvaluatesToConstantThrows
+    {
+        public ConditionEvaluatesToConstantThrows(object header, object body)
+        {
+            Header = header;
+            Body = body;
+        }
+
+        public object Header { get; }
+
+        public object Body { get; }
+
+
+        public static async Task<ConditionEvaluatesToConstantThrows> LoadAsync(Stream stream, Type headerType, Type bodyType)
+        {
+            var root = await XElement.LoadAsync(stream, LoadOptions.None, default);
+
+            var header = root.Element(root.Name.Namespace + nameof(Header))?
+                .Elements()
+                .FirstOrDefault()
+                .Deserialize(headerType);
+
+            var body = root.Element(root.Name.Namespace + nameof(Body))?
+                .Elements()
+                .FirstOrDefault()
+                .Deserialize(bodyType);
+
+            return new ConditionEvaluatesToConstantThrows(header, body);
+        }
+    }
+
+    public static class SoapMessageExtensions
+    {
+        public static object Deserialize(this XNode node, Type type)
+        {
+            return new object();
+        }
+    }
+}

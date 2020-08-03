82fca7d5601b4a4cbacea6ee0ea6cfb550e4af22
Corniel Nobel <mail@corniel.nl>
2020-08-03 14:05:18 +0200

    S3240 False positive on conditional operators.

---
+++ src/SonarAnalyzer.Experiments.CSharp/S3240.cs
@@ -0,0 +1,17 @@
+﻿namespace SonarAnalyzer.Experiments.CSharp
+{
+    public static class S3240
+    {
+        public static int NoConditionalOperator(int value)
+        {
+            if (value % 2 == 0) // none-compliant. Will lead to nested conditionals.
+            {
+                return value == 2 ? 1 : 0;
+            }
+            else
+            {
+                return 17;
+            }
+        }
+    }
+}

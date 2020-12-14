c4c34bd6b96ccf8efea13b9070427fdd78e9c34a
Corniel Nobel <mail@corniel.nl>
2020-12-14 22:10:55 +0100

    Added two issues.

--- src/SonarAnalyzer.Experiments.CSharp/S1172.cs
+++ src/SonarAnalyzer.Experiments.CSharp/S1172.cs
@@ -3,14 +3,14 @@ using System.Linq;
 
 namespace SonarAnalyzer.Experiments.CSharp
 {
-    public class S1172
+    public static class S1172
     {
         public static IEnumerable<(int, int)> Pairs()
         {
             return Enumerable.Range(0, 1).Select(i => PrivatePair(i, i * i));
         }
 
-        private static (int, int) PrivatePair(int l, int r) => (l, r); // compliant
+        private static (int, int) PrivatePair(int l, int r) => (l, r); // compliant FP
         public static (int, int) PublicPair(int l, int r) => (l, r); // compliant
     }
 }
---
+++ src/SonarAnalyzer.Experiments.CSharp/S1854.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace SonarAnalyzer.Experiments.CSharp
+{
+    public static class S1854
+    {
+        public static IEnumerable<string> Substrings(string str)
+        {
+            var start = 1; // compliant - FP
+            var end = 134; // compliant - FP
+            yield return str[start..];
+            yield return str[..end];
+        }
+    }
+}

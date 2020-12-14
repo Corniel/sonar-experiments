34083ae7449479d01e7e0ad7db6837114df98f1a
Corniel Nobel <mail@corniel.nl>
2020-12-14 21:50:40 +0100

    FP S1172

---
+++ src/SonarAnalyzer.Experiments.CSharp/S1172.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace SonarAnalyzer.Experiments.CSharp
+{
+    public class S1172
+    {
+        public static IEnumerable<(int, int)> Pairs()
+        {
+            return Enumerable.Range(0, 1).Select(i => PrivatePair(i, i * i));
+        }
+
+        private static (int, int) PrivatePair(int l, int r) => (l, r); // compliant
+        public static (int, int) PublicPair(int l, int r) => (l, r); // compliant
+    }
+}

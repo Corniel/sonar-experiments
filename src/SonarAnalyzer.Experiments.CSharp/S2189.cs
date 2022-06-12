8d213871dc94a6ea70517434f9509a38fc343971
Corniel Nobel <mail@corniel.nl>
2022-06-12 10:57:26 +0200

    S2198

---
+++ src/SonarAnalyzer.Experiments.CSharp/S2189.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+
+namespace SonarAnalyzer.Experiments.CSharp
+{
+    internal static class S2189
+    {
+        public static void For()
+        {
+            for (; ; )
+            {  // Noncompliant; end condition omitted
+               // ...
+            }
+        }
+
+        public static void While()
+        {
+            int j = 0;
+            while (true)
+            { // Noncompliant; end condition omitted
+                j++;
+            }
+        }
+
+        public static void Other()
+        { 
+            int k = 0;
+            bool b = true;
+            while (b)
+            { // Noncompliant; b never written to in loop
+                k++;
+            }
+        }
+
+        public static IEnumerable<string> Yield()
+        {
+            while (true)
+            {
+                yield return "test";
+            }
+        }
+    }
+}

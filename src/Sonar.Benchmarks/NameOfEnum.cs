c7e570cdb0d031f6b0f620e65c149102c504f18f
Corniel Nobel <mail@corniel.nl>
2023-03-31 11:39:45 +0200

    Add benchmark.

---
+++ src/Sonar.Benchmarks/NameOfEnum.cs
@@ -0,0 +1,14 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace Sonar.Benchmarks;
+
+public class NameOfEnum
+{
+    enum Enumeration { SomeValue };
+
+    [Benchmark(Description = "ToString()")]
+    public string WithToString() => $"Prefix:{Enumeration.SomeValue}";
+
+    [Benchmark(Description = "nameof()")]
+    public string WithNameOf() => $"Prefix:{nameof(Enumeration.SomeValue)}";
+}
---
+++ src/Sonar.Benchmarks/Program.cs
@@ -0,0 +1,11 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace Sonar.Benchmarks;
+
+internal static class Program
+{
+    static void Main(string[] args)
+    {
+        _ = BenchmarkRunner.Run<NameOfEnum>();
+    }
+}

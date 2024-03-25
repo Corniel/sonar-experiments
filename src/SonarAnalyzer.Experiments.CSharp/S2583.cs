bedb945023aa2f22d603345cc3b5323286dbf66d
Corniel Nobel <corniel@gmail.com>
2024-03-25 10:07:21 +0100

    Add S2583

--- src/SonarAnalyzer.Experiments.CSharp/S2583.cs
+++ src/SonarAnalyzer.Experiments.CSharp/S2583.cs
@@ -1,18 +1,88 @@
-﻿namespace SonarAnalyzer.Experiments.CSharp
+﻿using Microsoft.VisualBasic;
+
+namespace SonarAnalyzer.Experiments.CSharp;
+
+public class S2583
 {
-    public class S2583
+    public int FalsePositiveOnStringEmpty(string input)
     {
-        public int FalsePositiveOnStringEmpty(string input)
+        if(input == null)
         {
-            if(input == null)
-            {
-                return -1;
-            }
-            if (string.IsNullOrEmpty(input)) // compliant. String.Empty is different than (string)null.
-            {
-                return -2;
-            }
-            return 0;
+            return -1;
         }
+        if (string.IsNullOrEmpty(input)) // compliant. String.Empty is different than (string)null.
+        {
+            return -2;
+        }
+        return 0;
+    }
+
+    public int PlusPlusTakenIntoAccount2(string str)
+    {
+        for (var index = 0; index < str.Length; index++)
+        {
+            if (index % 4 == 0) // Compliant.
+            {
+                return 42;
+            }
+        }
+        return 0;
+    }
+
+    public int plusPlusNotTakenIntoAccount2(string str)
+    {
+        var index = 0;
+        while (index++ < str.Length)
+        {
+            if (index % 4 == 0) // Compliant.
+            {
+                return 42;
+            }
+        }
+        return 0;
+    }
+
+    public int plusPlusNotTakenIntoAccount(string str)
+    {
+        var index = 0;
+        while (index < str.Length)
+        {
+            index++;
+            if (index % 4 == 0) // Compliant.
+            {
+                return 42;
+            }
+        }
+        return 0;
+    }
+
+    public int plusPlusNotTakenIntoAccount()
+    {
+        var str = "Hello, World";
+        var index = 0;
+        while (index < str.Length)
+        {
+            index++;
+            if (index % 4 == 0) // Compliant
+            {
+                return 42;
+            }
+        }
+        return 0;
+    }
+
+    public int plusPlusNotTakenIntoAccountConst()
+    {
+        var str = "42";
+        var index = 0;
+        while (index < str.Length)
+        {
+            index++;
+            if (index % 4 == 0) // Noncompliant, index will be between 1 and 3.
+            {
+                return 42;
+            }
+        }
+        return 0;
     }
 }

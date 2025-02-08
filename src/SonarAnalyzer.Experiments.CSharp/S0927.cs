47ab3bcc94ebf7e8475324112337e930fe0c52a9
Corniel Nobel <corniel@gmail.com>
2025-02-08 21:11:56 +0100

    Add S927 issue

---
+++ src/SonarAnalyzer.Experiments.CSharp/S0927.cs
@@ -0,0 +1,20 @@
+﻿ namespace S0927;
+
+public interface IFace
+{
+	void Foo(string fooString);
+	void Foo(System.Text.RegularExpressions.Regex fooRegex);
+}
+
+public class MyImpl : IFace
+{
+	public void Foo(string barString) // NONCOMPLIANT with S927
+	{
+		// [...]
+	}
+
+	public void Foo(System.Text.RegularExpressions.Regex barRegex) // NONCOMPLIANT with S927
+	{
+		// [...]
+	}
+}

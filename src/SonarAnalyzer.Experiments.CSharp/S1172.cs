0f684dae087ad979ae61fcb1645b43118a95cd70
Corniel Nobel <corniel@gmail.com>
2025-12-15 10:47:19 +0100

    Add S1172 case

--- src/SonarAnalyzer.Experiments.CSharp/S1172.cs
+++ src/SonarAnalyzer.Experiments.CSharp/S1172.cs
@@ -1,16 +1,27 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
-namespace SonarAnalyzer.Experiments.CSharp
-{
-    public static class S1172
-    {
-        public static IEnumerable<(int, int)> Pairs()
-        {
-            return Enumerable.Range(0, 1).Select(i => PrivatePair(i, i * i));
-        }
+namespace S1172;
 
-        private static (int, int) PrivatePair(int l, int r) => (l, r); // compliant FP
-        public static (int, int) PublicPair(int l, int r) => (l, r); // compliant
-    }
+public class Compliant
+{
+	public static IEnumerable<(int, int)> Pairs()
+	{
+		return Enumerable.Range(0, 1).Select(i => PrivatePair(i, i * i));
+	}
+
+	private static (int, int) PrivatePair(int l, int r) => (l, r); // compliant FP
+	
+	public static (int, int) PublicPair(int l, int r) => (l, r); // compliant
+
+	private static long InlineUsage(int value) // FP, value is used by the inline function.
+	{
+		return Inline();
+
+		int Inline()
+		{
+			return Random.Shared.Next() > 10 ? value : 42;
+		}
+	}
 }

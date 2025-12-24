53e2a2dcb1a684d10a33976f625c3e1559c15ec8
Corniel Nobel <corniel@gmail.com>
2025-12-24 14:48:13 +0100

    Ad FP for S3626

---
+++ src/SonarAnalyzer.Experiments.CSharp/S3626.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace S3626;
+
+public class NonCompliant
+{
+    void Continue(IEnumerable<int> numbers)
+    {
+        foreach (var n in numbers)
+        {
+            if (n is not 42) continue; // FN, is not different from n != 42.
+        }
+    }
+}

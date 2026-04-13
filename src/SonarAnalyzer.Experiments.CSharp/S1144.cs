c2c34dc9adfc6dc342d0f5261e682db54e8d3d01
Corniel Nobel <corniel@gmail.com>
2026-04-13 15:44:04 +0200

    Add S1144 case

--- src/SonarAnalyzer.Experiments.CSharp/S1144.cs
+++ src/SonarAnalyzer.Experiments.CSharp/S1144.cs
@@ -1,54 +1,81 @@
-using System.Diagnostics;
-using System.Text.RegularExpressions;
+using System;
+using System.Diagnostics.Contracts;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
-public class Applicable
+namespace S1144;
+
+/// <summary>
+/// Provides a base implementation for JSON converters of structs that may or may not be nullable.
+/// </summary>
+/// <typeparam name="T">The type of structs to convert.</typeparam>
+public class NullableStructJsonConverter<T> : JsonConverterFactory
+	where T : struct
 {
-	public int Value(int value) => new Durs(value)[3];
+	private static readonly JsonSerializerOptions FallbackOptions = new(JsonSerializerDefaults.Web);
+	private readonly Nullable nullable;
+	private readonly NotNullable notNullable;
 
-	[DebuggerDisplay("{this[0]}, {this[1]}, {this[2]}, {this[3]}, {this[4]}")]
-	readonly record struct Durs(int Value)
+	/// <summary>
+	/// Initializes a new instance of the <see cref="NullableStructJsonConverter{T}"/> class.
+	/// </summary>
+	protected NullableStructJsonConverter()
 	{
-		public int this[int pos] => (Value >> (pos << 2)) & 7; // FP: Called by both Value as DebuggerDisplay.
+		nullable = new(this);
+		notNullable = new(this);
 	}
-}
 
-namespace SonarAnalyzer.Experiments.CSharp
-{
-    public class S1144
-    {
-        public void M()
-        {
-            OtherClass oc = new OtherClass();
-            (oc.Name, oc.Type) = ("A", "B");
-        }
+	/// <inheritdoc />
+	[Pure]
+	public override bool CanConvert(Type typeToConvert)
+		=> typeToConvert == typeof(T) || typeToConvert == typeof(T?);
 
-        class OtherClass
-        {
-            public string Name { get; set; } // Compliant, usage via a TupleExpression should be recognized.
-            public string Type { get; set; } // Compliant, usage via a TupleExpression should be recognized.
-        }
-    }
-
-	internal class LogConverter
+	/// <inheritdoc />
+	[Pure]
+	public override JsonConverter? CreateConverter(Type typeToConvert, JsonSerializerOptions options) => this switch
 	{
-		private LogConverter(string regex, string alertType)
-		{
-			_regex = new Regex(regex);
-			AlertType = alertType;
-		}
+		_ when typeToConvert == typeof(T) => notNullable,
+		_ when typeToConvert == typeof(T?) => nullable,
+		_ => null,
+	};
 
-		public string AlertType { get; }
-		private readonly Regex _regex;
+	/// <inheritdoc cref="JsonConverter{T}.Read(ref Utf8JsonReader, Type, JsonSerializerOptions)" />
+	[Pure]
+	public virtual T Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+		=> ReadNullable(ref reader, typeToConvert, options) ?? default;
 
-		public bool IsMatch(string message)
-		{
-			return _regex.IsMatch(message);
-		}
+	/// <inheritdoc cref="Read(ref Utf8JsonReader, Type, JsonSerializerOptions)" />
+	[Pure]
+	public virtual T? ReadNullable(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+		=> JsonSerializer.Deserialize<T?>(ref reader, FallbackOptions);
 
-		public static readonly LogConverter[] Converters =
-		[
-			new("^AGV route completed", "0xAAFF0001"),
-			new("^AGV route failed during execution", "0xAAFF0002"),
-		];
+	/// <inheritdoc cref="JsonConverter{T}.Write(Utf8JsonWriter, T, JsonSerializerOptions)" />
+	public virtual void Write(Utf8JsonWriter writer, T value, JsonSerializerOptions options)
+		=> JsonSerializer.Serialize(writer, value, FallbackOptions);
+
+	/// <inheritdoc cref="Write(Utf8JsonWriter, T, JsonSerializerOptions)" />
+	public virtual void Write(Utf8JsonWriter writer, T? value, JsonSerializerOptions options)
+		=> JsonSerializer.Serialize(writer, value, FallbackOptions);
+
+	private sealed class NotNullable(NullableStructJsonConverter<T> converter) : JsonConverter<T>
+	{
+		/// <inheritdoc />
+		public override T Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+			=> converter.Read(ref reader, typeToConvert, options);
+
+		/// <inheritdoc />
+		public override void Write(Utf8JsonWriter writer, T value, JsonSerializerOptions options)
+			=> converter.Write(writer, value, FallbackOptions);
+	}
+
+	private sealed class Nullable(NullableStructJsonConverter<T> converter) : JsonConverter<T?>
+	{
+		/// <inheritdoc />
+		public override T? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+			=> converter.ReadNullable(ref reader, typeToConvert, options);
+
+		/// <inheritdoc />
+		public override void Write(Utf8JsonWriter writer, T? value, JsonSerializerOptions options)
+			=> converter.Write(writer, value, FallbackOptions);
 	}
 }

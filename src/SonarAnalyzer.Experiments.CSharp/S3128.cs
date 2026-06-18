8696769db3d9fbfe5e572c7b783f71b94fd47c58
Corniel Nobel <corniel@gmail.com>
2026-06-18 10:34:00 +0200

    Add FP S3125

---
+++ src/SonarAnalyzer.Experiments.CSharp/S3128.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace S3125;
+
+public class Implementation
+{
+	public static readonly object Do = new();
+
+	private sealed class Nil : IContract
+	{
+		public void Do() => throw new NotSupportedException();
+	}
+}
+
+public interface IContract
+{
+	void Do();
+}

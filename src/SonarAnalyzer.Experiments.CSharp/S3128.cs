583f9bac0b3d3cbf293f9918f2ad414e4be8bb49
Corniel Nobel <corniel@gmail.com>
2026-06-18 10:41:24 +0200

    extend

--- src/SonarAnalyzer.Experiments.CSharp/S3128.cs
+++ src/SonarAnalyzer.Experiments.CSharp/S3128.cs
@@ -2,17 +2,26 @@
 
 namespace S3125;
 
-public class Implementation
+public class Container
 {
 	public static readonly object Do = new();
 
-	private sealed class Nil : IContract
+	public static readonly object Other = new();
+
+	private sealed class PrivateImplementation : Base, IContract
 	{
-		public void Do() => throw new NotSupportedException();
+		public void Do() { } // Compliant, enforced by the interface
+
+		public override void Other() { } // Compliant, enforced by the interface
 	}
 }
 
 public interface IContract
 {
 	void Do();
+}
+
+public abstract class Base
+{
+	public abstract void Other();
 }

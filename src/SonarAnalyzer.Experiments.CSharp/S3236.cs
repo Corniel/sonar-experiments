28372b5ed9aaba3a4d8053176e90bb41f9870dca
Corniel Nobel <corniel@gmail.com>
2026-07-13 16:02:01 +0200

    S3236

---
+++ src/SonarAnalyzer.Experiments.CSharp/S3236.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace S3236;
+
+public class MyModel
+{
+    public MyModel(int? total = null)
+    {
+		NotNegative(total ?? 0, nameof(total)); // FP: without nameof() "total ?? 0" is provided as paramName.
+		Total = total;
+    }
+
+	public int? Total { get; init; }
+
+	private static int NotNegative(int parameter, [CallerArgumentExpression(nameof(parameter))] string? paramName = null)
+	=> parameter < 0
+		? throw new ArgumentOutOfRangeException(paramName, "Value should not be negative.")
+		: parameter;
+}

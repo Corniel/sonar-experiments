e2c5e073897a2a4a21d17299a627c79797ba87c4
Corniel Nobel <corniel@gmail.com>
2026-09-23 13:23:39 +0200

    Add S8949 reproducer

---
+++ src/SonarAnalyzer.Experiments.CSharp/S8949.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Diagnostics.Contracts;
+using System.Threading;
+using System.Threading.Tasks;
+
+public static class S8949
+{
+	/// <summary>Gets the diagnostics for the specified analyzers.</summary>
+	[Pure]
+	public static async Task<IReadOnlyCollection<Diagnostic>> GetDiagnosticsAsync(
+		Compilation compilation,
+		ImmutableArray<DiagnosticAnalyzer> analyzers,
+		IEnumerable<AdditionalText> texts,
+		CancellationToken cancellationToken = default)
+	{
+		var options = compilation.Options.WithSpecificDiagnosticOptions([]);
+		var analyzerOptions = new AnalyzerOptions([.. texts]);
+
+		var diagnostics = await compilation // FP, cancelation token is passed.
+			.WithOptions(options) 
+			.WithAnalyzers(analyzers, analyzerOptions)
+			.GetAllDiagnosticsAsync(cancellationToken);
+
+		return diagnostics;
+	}
+}

37cfd06efdbdff0397db573bf85caf5d3cc0638b
Corniel Nobel <corniel@gmail.com>
2026-09-23 12:58:23 +0200

    SQ0079

---
+++ src/SonarAnalyzer.Experiments.CSharp/SQ0079.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace SQ0079;
+
+public static class Guids
+{
+	#pragma warning disable S4790
+	public static Guid MD5Hash(byte[] data)
+	{
+		var hash = MD5.HashData(data); // Suppressed S4790
+		return new Guid(hash);
+	}
+}

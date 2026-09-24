e2b4939370b1a4e67edeabca131b98237a933855
Corniel Nobel <corniel@gmail.com>
2026-09-24 13:50:43 +0200

    New S1144 FP

--- src/SonarAnalyzer.Experiments.CSharp/S1144.cs
+++ src/SonarAnalyzer.Experiments.CSharp/S1144.cs
@@ -1,81 +1,34 @@
-using System;
-using System.Diagnostics.Contracts;
-using System.Text.Json;
-using System.Text.Json.Serialization;
+using System.Collections.Generic;
+using System.IO;
 
 namespace S1144;
 
-/// <summary>
-/// Provides a base implementation for JSON converters of structs that may or may not be nullable.
-/// </summary>
-/// <typeparam name="T">The type of structs to convert.</typeparam>
-public class NullableStructJsonConverter<T> : JsonConverterFactory
-	where T : struct
+public class ResourceCollection
 {
-	private static readonly JsonSerializerOptions FallbackOptions = new(JsonSerializerDefaults.Web);
-	private readonly Nullable nullable;
-	private readonly NotNullable notNullable;
+	private readonly Dictionary<string, Dictionary<string, string?>> resources = [];
 
-	/// <summary>
-	/// Initializes a new instance of the <see cref="NullableStructJsonConverter{T}"/> class.
-	/// </summary>
-	protected NullableStructJsonConverter()
+	public Dictionary<string, string?> this[string key]
 	{
-		nullable = new(this);
-		notNullable = new(this);
+		get
+		{
+			if (!resources.TryGetValue(key, out var resoures))
+			{
+				resoures = [];
+				resources[key] = resoures;
+			}
+			return resoures;
+		}
+		private set => resources[key] = value; // FP, used by line 31.
 	}
 
-	/// <inheritdoc />
-	[Pure]
-	public override bool CanConvert(Type typeToConvert)
-		=> typeToConvert == typeof(T) || typeToConvert == typeof(T?);
-
-	/// <inheritdoc />
-	[Pure]
-	public override JsonConverter? CreateConverter(Type typeToConvert, JsonSerializerOptions options) => this switch
+	public static ResourceCollection Load(DirectoryInfo dir)
 	{
-		_ when typeToConvert == typeof(T) => notNullable,
-		_ when typeToConvert == typeof(T?) => nullable,
-		_ => null,
-	};
+		var collection = new ResourceCollection();
 
-	/// <inheritdoc cref="JsonConverter{T}.Read(ref Utf8JsonReader, Type, JsonSerializerOptions)" />
-	[Pure]
-	public virtual T Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
-		=> ReadNullable(ref reader, typeToConvert, options) ?? default;
-
-	/// <inheritdoc cref="Read(ref Utf8JsonReader, Type, JsonSerializerOptions)" />
-	[Pure]
-	public virtual T? ReadNullable(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
-		=> JsonSerializer.Deserialize<T?>(ref reader, FallbackOptions);
-
-	/// <inheritdoc cref="JsonConverter{T}.Write(Utf8JsonWriter, T, JsonSerializerOptions)" />
-	public virtual void Write(Utf8JsonWriter writer, T value, JsonSerializerOptions options)
-		=> JsonSerializer.Serialize(writer, value, FallbackOptions);
-
-	/// <inheritdoc cref="Write(Utf8JsonWriter, T, JsonSerializerOptions)" />
-	public virtual void Write(Utf8JsonWriter writer, T? value, JsonSerializerOptions options)
-		=> JsonSerializer.Serialize(writer, value, FallbackOptions);
-
-	private sealed class NotNullable(NullableStructJsonConverter<T> converter) : JsonConverter<T>
-	{
-		/// <inheritdoc />
-		public override T Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
-			=> converter.Read(ref reader, typeToConvert, options);
-
-		/// <inheritdoc />
-		public override void Write(Utf8JsonWriter writer, T value, JsonSerializerOptions options)
-			=> converter.Write(writer, value, FallbackOptions);
-	}
-
-	private sealed class Nullable(NullableStructJsonConverter<T> converter) : JsonConverter<T?>
-	{
-		/// <inheritdoc />
-		public override T? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
-			=> converter.ReadNullable(ref reader, typeToConvert, options);
-
-		/// <inheritdoc />
-		public override void Write(Utf8JsonWriter writer, T? value, JsonSerializerOptions options)
-			=> converter.Write(writer, value, FallbackOptions);
+		foreach (var file in dir.GetFiles($"*.resx"))
+		{
+			collection[Path.GetFileNameWithoutExtension(file.Name)] = [];
+		}
+		return collection;
 	}
 }
